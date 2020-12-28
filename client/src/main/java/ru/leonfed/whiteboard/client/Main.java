85c7bec71b5c0dbd4a8a2b524d4f3144bcd17fe3
fedleonid <fedleonid@yandex-team.ru>
2020-12-28 23:34:22 +0300

    add core, client, server modules

---
+++ client/src/main/java/ru/leonfed/whiteboard/client/Main.java
@@ -0,0 +1,8 @@
+package ru.leonfed.whiteboard.client;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("It doesn't do anything :/");
+    }
+}
---
+++ server/src/main/java/ru/leonfed/whiteboard/server/Main.java
@@ -0,0 +1,8 @@
+package ru.leonfed.whiteboard.server;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("It doesn't do anything :/");
+    }
+}
