a38a356907a68e861d9d1e4d548a598d3c06572f
fedleonid <fedleonid@yandex-team.ru>
2020-12-31 15:59:34 +0300

    rename Main to ServerInitializer

--- server/src/main/java/ru/leonfed/whiteboard/server/ServerInitializer.java
+++ server/src/main/java/ru/leonfed/whiteboard/server/ServerInitializer.java
@@ -1,6 +1,6 @@
 package ru.leonfed.whiteboard.server;
 
-public class Main {
+public class ServerInitializer {
 
     public static void main(String[] args) {
         System.out.println("It doesn't do anything :/");
